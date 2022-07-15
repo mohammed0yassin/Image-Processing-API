e884b3a9547cf33e4df2a196c8887c386bb8f3e6
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-07-16 01:02:28 +0200

    feat: error handling (missing image)

--- src/routes/api/images/images.ts
+++ src/routes/api/images/images.ts
@@ -1,3 +1,4 @@
+import { AssertionError } from 'assert';
 import express from 'express';
 import path from 'path';
 
@@ -13,22 +14,32 @@ const imageExists = async (imageThumbnail: string, width: number, height: number
 
 
 images.get('/', async (req, res) => {
-  // Get image and thumbnail details
-  const imagePath = path.join(process.cwd(), '/full/' , (req.query.filename as unknown) as string);
-  const width = parseInt((req.query.width as unknown) as string);
-  const height = parseInt((req.query.height as unknown) as string);
-  const imageThumbnail = path.join(process.cwd(), '/thumb/' , (req.query.filename as unknown) as string);
-  // Check if imageThumbnail exists
-  const imageExist = await imageExists(imageThumbnail, width, height);
-  if (imageExist) {
-    console.log(`Image ${req.query.filename} already exists`)
-    res.sendFile(imageThumbnail);
-  }
-  else {
-    const resizedImage = await sharp(imagePath).resize(width, height).toFile(imageThumbnail);
-    console.log(`Image ${req.query.filename} resized`)
-    res.sendFile(imageThumbnail);
-  }
+  try {
+    // Get image and thumbnail details
+    const imagePath = path.join(process.cwd(), '/full/' , (req.query.filename as unknown) as string);
+    const width = parseInt((req.query.width as unknown) as string);
+    const height = parseInt((req.query.height as unknown) as string);
+    const imageThumbnail = path.join(process.cwd(), '/thumb/' , (req.query.filename as unknown) as string);
+    // Check if imageThumbnail exists
+    const imageExist = await imageExists(imageThumbnail, width, height);
+    if (imageExist) {
+      console.log(`Image ${req.query.filename} already exists`)
+      res.sendFile(imageThumbnail);
+    }
+    // Else create new thumbnail
+    else {
+      const resizedImage = await sharp(imagePath).resize(width, height).toFile(imageThumbnail);
+      console.log(`Image ${req.query.filename} resized`)
+      res.sendFile(imageThumbnail);
+    }
+  } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.log(err.stack);
+        res.send(`Image ${req.query.filename} does not exist`);
+      } else {
+        console.log('Unexpected error', err);
+      }
+  };
 });
 
 export default images;
