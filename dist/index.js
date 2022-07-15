bf18db119dbedd1c4b45b694314421b948bf1d56
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-07-15 19:40:32 +0200

    chore: init environment

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var developer;
+developer = "Yassin";
+var GreetDeveloper = function (developerName) {
+    console.log("Development by ".concat(developerName));
+};
+GreetDeveloper(developer);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let developer: string;
+
+developer = "Yassin"
+
+const GreetDeveloper = (developerName: string): void => {
+  console.log(`Development by ${developerName}`);
+} 
+
+GreetDeveloper(developer);
